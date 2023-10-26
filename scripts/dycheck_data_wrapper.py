f53f7bee5767ed4132a991981306f9bcd519129f
FarukUgurcali <f.ugurcali@gmail.com>
2023-10-26 04:15:14 +0300

    Nerfplayer data preperation script, Colmap calibration method added. There is an issue with the scale when training the model with colmap data.

--- scripts/dycheck_data_wrapper.py
+++ scripts/dycheck_data_wrapper.py
@@ -1,8 +1,5 @@
 import json
 import os
-import shutil
-
-import numpy as np
 from PIL import Image
 
 #This script is a wrapper for nerfplayer dycheck data parser, which gets spaceport cage data and calibration infos
@@ -17,6 +14,7 @@ with open('config.json', 'r') as f:
 DATA_DIR_IN = config['DATA_DIR_IN']
 DATA_DIR_OUT = config['DATA_DIR_OUT']
 JSON_DIR_OUT = config['JSON_DIR_OUT']
+CALIB_METHOD = config['CALIB_METHOD']
 CALIB_JSON_DIR = config['CALIB_JSON_DIR']
 
 
@@ -64,14 +62,6 @@ def rename_images(image_dir, output_dir):
     if not os.path.exists(img_save_dir):
         os.makedirs(img_save_dir)
 
-    """# Iterate over the sorted file details and copy the files with new names
-    for i, (filename, serial_number, frame_idx) in enumerate(file_details):
-        frame_idx_str = str(frame_idx - frame_idx_offset).zfill(5)
-        new_cam_idx = str(serial_number_mapping[serial_number]).zfill(2)
-        new_filename = f"{new_cam_idx}_{frame_idx_str}.png"
-        shutil.copy(os.path.join(image_dir, filename), os.path.join(output_dir, new_filename))
-        print(f"Copied {filename} to {new_filename}")"""
-    
     # Iterate over the sorted file details and save the images with new names
     for i, (filename, serial_number, frame_idx) in enumerate(file_details):
         frame_idx_str = str(frame_idx - frame_idx_offset).zfill(5)
@@ -188,7 +178,7 @@ def create_dataset_jsons(modified_data_path, json_save_dir):
 
     return
 
-def create_camera_jsons(calibration_file, serial_num_mapping, rgb_input_path, output_jsons_path):
+def create_camera_jsons(calibration_file, serial_num_mapping, rgb_input_path, output_jsons_path, calib_method):
     """
     Create camera-related JSON files using calibration data and serial number mappings.
 
@@ -197,6 +187,7 @@ def create_camera_jsons(calibration_file, serial_num_mapping, rgb_input_path, ou
     - serial_num_mapping (dict): Mapping of serial numbers.
     - rgb_input_path (str): Path to input RGB data.
     - output_jsons_path (str): Directory where output JSON files will be saved.
+    - calib_method (str): Calibration method used to generate the calibration data.
     """
     
     # Check if output directory exists, if not, create it
@@ -207,93 +198,148 @@ def create_camera_jsons(calibration_file, serial_num_mapping, rgb_input_path, ou
     # Load the calibration data
     with open(calibration_file, 'r') as f:
         calibration_data = json.load(f)
-
-    # Create a dictionary to map filenames to their calibration data
-    filename_to_calibration = {}
-    for frame_data in calibration_data["frames"]:
-        filename = os.path.basename(frame_data["file_path"])
-        filename_to_calibration[filename] = frame_data
-
     rgb_input_path = os.path.join(rgb_input_path, '1x')
-    # Iterate over each image in the rgb_input_path
-    for image_name in os.listdir(rgb_input_path):
-        if image_name.endswith('.png'):
-            # Get the cam_id from the image name
-            cam_id = image_name.split('_')[0]
-            
-            # Map the cam_id to a serial number using the serial_num_mapping
-            serial_number = None
-            for key, value in serial_num_mapping.items():
-                if value == int(cam_id):
-                    serial_number = key
-                    break
-            
-            # If we couldn't find a serial number, skip this image
-            if serial_number is None:
-                continue
-            
-            # Deduce the expected filename from the serial number and frame index
-            frame_idx = image_name.split('_')[1].split('.')[0]  # Get frame index from the image name
-            expected_filename = f"ts_100_{serial_number}_w1920_h1200.jpg"  # This assumes a specific naming pattern. Adjust if necessary.
 
-            frame_data_new = None
-            # Fetch the corresponding calibration data
-            for frame_data in calibration_data["frames"]:
-                if expected_filename == frame_data['file_path']:
-                    #frame_calibration = filename_to_calibration.get(expected_filename, None)
-                    frame_data_new = {}
-                    frame_data_new["fl_x"] = frame_data["fl_x"]
-                    frame_data_new["fl_y"] = frame_data["fl_y"]
-                    frame_data_new["w"] = frame_data["w"]
-                    frame_data_new["h"] = frame_data["h"]
-                    frame_data_new["cx"] = frame_data["cx"]
-                    frame_data_new["cy"] = frame_data["cy"]
-                    frame_data_new["k1"] = frame_data["k1"]
-                    frame_data_new["k2"] = frame_data["k2"]
-                    frame_data_new["k3"] = frame_data["k3"]
-                    frame_data_new["k4"] = frame_data["k4"]
-                    frame_data_new["p1"] = frame_data["p1"]
-                    frame_data_new["p2"] = frame_data["p2"]
-                    frame_data_new["transform_matrix"] = frame_data["transform_matrix"]
-                    frame_data_new["camera_model"] = frame_data["camera_model"]
+    if calib_method == 'MultiCamCalibTool':
+        # Create a dictionary to map filenames to their calibration data
+        filename_to_calibration = {}
+        for frame_data in calibration_data["frames"]:
+            filename = os.path.basename(frame_data["file_path"])
+            filename_to_calibration[filename] = frame_data
 
-            # If we couldn't find calibration data, skip this image
-            if frame_data_new is None:
-                continue
-            
-            transformed_calibration = {
-                "transform_matrix": frame_data_new["transform_matrix"], # 4x4 matrix
-                "fl_x": frame_data_new["fl_x"], # float
-                "fl_y": frame_data_new["fl_y"], # float
-                "c_x": frame_data_new["cx"], # float
-                "c_y": frame_data_new["cy"], # float
-                "h": frame_data_new["h"], # int
-                "w": frame_data_new["w"], # int
-                "k1": frame_data_new["k1"], # float
-                "k2": frame_data_new["k2"], # float
-                "k3": frame_data_new["k3"], # float
-                "k4": frame_data_new["k4"], # float
-                "p1": frame_data_new["p1"], # float
-                "p2": frame_data_new["p2"], # float
-                "camera_model": frame_data_new["camera_model"] # string
-            }
-            # Create the new JSON filename
-            json_filename = f"{cam_id}_{frame_idx}.json"
-            
-            # Write the calibration data for this frame to the new file
-            with open(os.path.join(output_jsons_path, json_filename), 'w') as f:
-                json.dump(transformed_calibration, f, indent=4)
+        # Iterate over each image in the rgb_input_path
+        for image_name in os.listdir(rgb_input_path):
+            if image_name.endswith('.png'):
+                # Get the cam_id from the image name
+                cam_id = image_name.split('_')[0]
+
+                # Map the cam_id to a serial number using the serial_num_mapping
+                serial_number = None
+                for key, value in serial_num_mapping.items():
+                    if value == int(cam_id):
+                        serial_number = key
+                        break
+
+                # If we couldn't find a serial number, skip this image
+                if serial_number is None:
+                    continue
+
+                # Deduce the expected filename from the serial number and frame index
+                frame_idx = image_name.split('_')[1].split('.')[0]  # Get frame index from the image name
+                expected_filename = f"ts_100_{serial_number}_w1920_h1200.jpg"  # This assumes a specific naming pattern. Adjust if necessary.
+
+                frame_data_new = None
+                # Fetch the corresponding calibration data
+                for frame_data in calibration_data["frames"]:
+                    if expected_filename == frame_data['file_path']:
+                        #frame_calibration = filename_to_calibration.get(expected_filename, None)
+                        frame_data_new = {}
+                        frame_data_new["fl_x"] = frame_data["fl_x"]
+                        frame_data_new["fl_y"] = frame_data["fl_y"]
+                        frame_data_new["w"] = frame_data["w"]
+                        frame_data_new["h"] = frame_data["h"]
+                        frame_data_new["cx"] = frame_data["cx"]
+                        frame_data_new["cy"] = frame_data["cy"]
+                        frame_data_new["k1"] = frame_data["k1"]
+                        frame_data_new["k2"] = frame_data["k2"]
+                        frame_data_new["k3"] = frame_data["k3"]
+                        frame_data_new["k4"] = frame_data["k4"]
+                        frame_data_new["p1"] = frame_data["p1"]
+                        frame_data_new["p2"] = frame_data["p2"]
+                        frame_data_new["transform_matrix"] = frame_data["transform_matrix"]
+                        frame_data_new["camera_model"] = frame_data["camera_model"]
+
+                # If we couldn't find calibration data, skip this image
+                if frame_data_new is None:
+                    continue
+
+                transformed_calibration = {
+                    "transform_matrix": frame_data_new["transform_matrix"], # 4x4 matrix
+                    "fl_x": frame_data_new["fl_x"], # float
+                    "fl_y": frame_data_new["fl_y"], # float
+                    "c_x": frame_data_new["cx"], # float
+                    "c_y": frame_data_new["cy"], # float
+                    "h": frame_data_new["h"], # int
+                    "w": frame_data_new["w"], # int
+                    "k1": frame_data_new["k1"], # float
+                    "k2": frame_data_new["k2"], # float
+                    "k3": frame_data_new["k3"], # float
+                    "k4": frame_data_new["k4"], # float
+                    "p1": frame_data_new["p1"], # float
+                    "p2": frame_data_new["p2"], # float
+                    "camera_model": frame_data_new["camera_model"] # string
+                }
+                # Create the new JSON filename
+                json_filename = f"{cam_id}_{frame_idx}.json"
+
+                # Write the calibration data for this frame to the new file
+                with open(os.path.join(output_jsons_path, json_filename), 'w') as f:
+                    json.dump(transformed_calibration, f, indent=4)
+
+    if calib_method == 'Colmap':
+        # Iterate over each image in the rgb_input_path
+        for image_name in os.listdir(rgb_input_path):
+            if image_name.endswith('.png'):
+                # Get the cam_id from the image name
+                cam_id = image_name.split('_')[0]
+                frame_idx = image_name.split('_')[1].split('.')[0]  # Get frame index from the image name
+                # fname_json should be in format images/00001.json
+                fname_json = f"images/frame_{int(cam_id)+1:05}.jpg"
+                frame_data_new = None
+                # Fetch the corresponding calibration data
+                for frame_data in calibration_data["frames"]:
+                    if fname_json == frame_data['file_path']:
+                        # frame_calibration = filename_to_calibration.get(fname_json, None)
+                        frame_data_new = {}
+                        frame_data_new["fl_x"] = calibration_data["fl_x"]
+                        frame_data_new["fl_y"] = calibration_data["fl_y"]
+                        frame_data_new["w"] = calibration_data["w"]
+                        frame_data_new["h"] = calibration_data["h"]
+                        frame_data_new["cx"] = calibration_data["cx"]
+                        frame_data_new["cy"] = calibration_data["cy"]
+                        frame_data_new["k1"] = calibration_data["k1"]
+                        frame_data_new["k2"] = calibration_data["k2"]
+                        frame_data_new["p1"] = calibration_data["p1"]
+                        frame_data_new["p2"] = calibration_data["p2"]
+                        frame_data_new["transform_matrix"] = frame_data["transform_matrix"]
+                        frame_data_new["camera_model"] = calibration_data["camera_model"]
+
+                # If we couldn't find calibration data, skip this image
+                if frame_data_new is None:
+                    continue
+
+                transformed_calibration = {
+                    "transform_matrix": frame_data_new["transform_matrix"],  # 4x4 matrix
+                    "fl_x": frame_data_new["fl_x"],  # float
+                    "fl_y": frame_data_new["fl_y"],  # float
+                    "c_x": frame_data_new["cx"],  # float
+                    "c_y": frame_data_new["cy"],  # float
+                    "h": frame_data_new["h"],  # int
+                    "w": frame_data_new["w"],  # int
+                    "k1": frame_data_new["k1"],  # float
+                    "k2": frame_data_new["k2"],  # float
+                    "p1": frame_data_new["p1"],  # float
+                    "p2": frame_data_new["p2"],  # float
+                    "camera_model": frame_data_new["camera_model"]  # string
+                }
+                # Create the new JSON filename
+                json_filename = f"{cam_id}_{frame_idx}.json"
+
+                # Write the calibration data for this frame to the new file
+                with open(os.path.join(output_jsons_path, json_filename), 'w') as f:
+                    json.dump(transformed_calibration, f, indent=4)
+
 
 def main():
-    serial_num_map = rename_images(image_dir=DATA_DIR_IN, output_dir=DATA_DIR_OUT)
+    #serial_num_map = rename_images(image_dir=DATA_DIR_IN, output_dir=DATA_DIR_OUT)
 
-    create_dataset_jsons(DATA_DIR_OUT, JSON_DIR_OUT)
+    #create_dataset_jsons(DATA_DIR_OUT, JSON_DIR_OUT)
 
     with open(os.path.join(DATA_DIR_OUT, 'serial_number_mapping.json'), 'r') as f:
         serial_num_map = json.load(f)
 
     cam_json_path = os.path.join(JSON_DIR_OUT, 'camera')
-    create_camera_jsons(CALIB_JSON_DIR, serial_num_map, DATA_DIR_OUT, cam_json_path)
+    create_camera_jsons(CALIB_JSON_DIR, serial_num_map, DATA_DIR_OUT, cam_json_path, CALIB_METHOD)
 
 
 if __name__ == "__main__":
